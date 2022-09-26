37f218848d0d97389c7cc003a93a822dbca17b9b
nikita2005553 <48449329+nikita2005553@users.noreply.github.com>
2022-09-26 15:20:40 +0300

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -50,7 +50,7 @@ while (summ < Math.Cos(x)) {
 double x, y;
 x = double.Parse(Console.ReadLine());
 y = double.Parse(Console.ReadLine());
-if ((x <= 1 && x >= 0 && y <= 1 && y >= 0) || (y >= 0 && y <= 1 && x >= -1 && x <= 0 && Math.Abs(x + y ) <= 0.5 || (x <= 0 && x >= -1 && y >= -1 && y<=0 && x * x + y * y <= 1)))
+if ((x <= 1 && x >= 0 && y <= 1 && y >= 0) || (y >= 0 && y <= 1 && x >= -1 && x <= 0 && Math.Abs(x)+ Math.Abs(y) <= 1) || (x <= 0 && x >= -1 && y >= -1 && y<=0 && x * x + y * y <= 1)))
 {
     Console.WriteLine(true);
 
