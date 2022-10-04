07679495b4d68da88a56fac64bf4c9acbfeea9cf
nikita2005553 <48449329+nikita2005553@users.noreply.github.com>
2022-10-04 09:50:59 +0300

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,11 +1,22 @@
-double x, y;
+double x, summ = 1; // summ = 1 так, как при k=0 правая часть равно 1 
 x = double.Parse(Console.ReadLine());
-y = double.Parse(Console.ReadLine());
-if ((x <= 1 && x >= 0 && y <= 1 && y >= 0) || (y >= 0 && y <= 1 && x >= -1 && x <= 0 && Math.Abs(x)+ Math.Abs(y) <= 1) || (x <= 0 && x >= -1 && y >= -1 && y<=0 && x * x + y * y <= 1))
-{
-    Console.WriteLine(true);
+double s = Math.Sqrt(x + 1);
+long i = -1, factorial2k = 1, factorialk = 1, step4 = 1, k = 1, k2 = 1;
+double stepx = x;
+double e = 0.01;
+while (s-summ > e){
+    //k=1
+    double numerator = 1;
+    double denominator = 1;
+    factorial2k *= k2 * (k2 + 1);
+    k2 += 2;
+    factorialk *= k;
+    step4 *= 4;
+    numerator = numerator * i * factorial2k;
+    denominator = (1 - 2 * k) * factorialk * factorialk * step4;
+    stepx *= x;
+    summ += (numerator / denominator) * stepx;
+}
+Console.WriteLine($"{s}, {summ}");
 
-}
-else { 
-    Console.WriteLine(false);  
-}
+
