705b9e59fc08dbb600f3467ceee0fa648514ba7b
nikita2005553 <48449329+nikita2005553@users.noreply.github.com>
2022-10-17 15:16:45 +0300

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,22 +1,43 @@
-double x, summ = 1; // summ = 1 так, как при k=0 правая часть равно 1 
-x = double.Parse(Console.ReadLine());
-double s = Math.Sqrt(x + 1);
-long i = -1, factorial2k = 1, factorialk = 1, step4 = 1, k = 1, k2 = 1;
-double stepx = x;
-double e = 0.01;
-while (s-summ > e){
-    //k=1
-    double numerator = 1;
-    double denominator = 1;
-    factorial2k *= k2 * (k2 + 1);
-    k2 += 2;
-    factorialk *= k;
-    step4 *= 4;
-    numerator = numerator * i * factorial2k;
-    denominator = (1 - 2 * k) * factorialk * factorialk * step4;
-    stepx *= x;
-    summ += (numerator / denominator) * stepx;
+Random r = new Random();
+int[] mass = new int[r.Next(10, 21)];
+int max = 0;
+int min = int.MaxValue;
+int indexmax = 0, indexmin=0;
+for (int i = 0; i < mass.Length; i++) {
+    mass[i] = r.Next(60);
+    max = Math.Max(max, mass[i]);
+    min = Math.Min(min, mass[i]);
 }
-Console.WriteLine($"{s}, {summ}");
 
+for (int i = 0; i < mass.Length; i++)
+{
+    if (mass[i] == max) { 
+        indexmax = i; break; 
+    }
+    if (mass[i] == min)
+    {
+        indexmin = i; break;
+    }
+}
 
+int summ = 0;
+if (indexmax < indexmin)
+{
+    for (int i = indexmax + 1; i < indexmin; i++)
+    {
+        summ += mass[i];
+    }
+}
+else {
+    for (int i = indexmin + 1; i < indexmax; i++)
+    {
+        summ += mass[i];
+    }
+}
+
+Console.WriteLine(summ);
+
+for (int i = 0; i < mass.Length; i++)
+{
+    Console.Write($"{mass[i]} ");
+}
